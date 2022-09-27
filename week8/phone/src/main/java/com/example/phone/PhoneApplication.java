fd077db15b347522f449048affb3da8d3cb64cc5
Bakyt <Bakytbaz13@gmail.com>
2022-09-28 00:00:44 +0600

    mnmn

--- week8/phone/src/main/java/com/example/phone/PhoneApplication.java
+++ week8/phone/src/main/java/com/example/phone/PhoneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhoneApplication {
-
+int a=4;
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneApplication.class, args);
 	}
