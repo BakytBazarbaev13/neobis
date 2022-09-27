9a0f017913793a2d8cf0c90828c3c2ee11059df2
Bakyt <Bakytbaz13@gmail.com>
2022-09-28 00:01:36 +0600

    delete

--- week8/phone/src/main/java/com/example/phone/PhoneApplication.java
+++ week8/phone/src/main/java/com/example/phone/PhoneApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhoneApplication {
-int a=4;
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneApplication.class, args);
 	}
